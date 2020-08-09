4161eb21bb706c5d3efc93e8b2987e13a8b51048
arianna choza <unachoza@gmail.com>
2020-08-09 18:20:06 -0400

    sun and moon swap

--- script.js
+++ script.js
@@ -135,36 +135,16 @@ const drop = (e) => {
 
 updateDOM();
 const toggleSwitch = document.querySelector('input[type="checkbox"]');
-const nav = document.getElementById('nav');
 const toggleIcon = document.getElementById('toggle-icon');
-const image1 = document.getElementById('image1');
-const image2 = document.getElementById('image2');
-const image3 = document.getElementById('image3');
-const textBox = document.getElementById('text-box');
-
-// Dark or Light Images
-function imageMode(color) {
-  image1.src = `img/undraw_proud_coder_${color}.svg`;
-  image2.src = `img/undraw_feeling_proud_${color}.svg`;
-  image3.src = `img/undraw_conceptual_idea_${color}.svg`;
-}
 
 // Dark Mode Styles
 function darkMode() {
-  nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
-  textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
-  toggleIcon.children[0].textContent = 'Dark Mode';
-  toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
-  imageMode('dark');
+  toggleIcon.children[0].classList.replace('fa-sun', 'fa-moon');
 }
 
 // Light Mode Styles
 function lightMode() {
-  nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
-  textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
-  toggleIcon.children[0].textContent = 'Light Mode';
-  toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
-  imageMode('light');
+  toggleIcon.children[0].classList.replace('fa-moon', 'fa-sun');
 }
 
 // Switch Theme Dynamically
